0fa0ecee8b80afa8ee431f39e167ca488d809cc5
pradeep <pradeepmaurya.cse@gmail.com>
2022-09-20 02:47:11 +0530

    loginregistration-thy

--- src/main/java/com/hipradeep/code/UserRepository.java
+++ src/main/java/com/hipradeep/code/UserRepository.java
@@ -4,8 +4,15 @@ import com.hipradeep.code.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+
 	@Query("SELECT u FROM User u WHERE u.email = ?1")
 	public User findByEmail(String email);
+
+
+
 	
 }
