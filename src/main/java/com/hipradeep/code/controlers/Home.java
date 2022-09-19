4a98a0e7431bee272118078913d51dc8974d8a7a
pradeep <pradeepmaurya.cse@gmail.com>
2022-09-19 20:27:52 +0530

    Initial commit

---
+++ src/main/java/com/hipradeep/code/controlers/Home.java
@@ -0,0 +1,22 @@
+package com.hipradeep.code.controlers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    //http://localhost:8080/
+    @RequestMapping("/")
+    public String helloGFG()
+    {
+        return "Hello Pradeep";
+    }
+
+    //http://localhost:8080/welcome
+    @RequestMapping("/welcome")
+    public String homeAS()
+    {
+        return "Welcome Pradeep";
+    }
+}
