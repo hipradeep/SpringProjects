525740748477f5b07f5b2ee5413bdf7ae948d352
pradeep <pradeepmaurya.cse@gmail.com>
2022-09-20 01:04:45 +0530

    main

--- src/main/java/com/hipradeep/code/controllers/Home.java
+++ src/main/java/com/hipradeep/code/controllers/Home.java
@@ -1,4 +1,4 @@
-package com.hipradeep.code.controlers;
+package com.hipradeep.code.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
