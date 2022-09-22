93b9903e1f5c924a2f4598a3f656d62f5f52c726
pradeep <pradeepmaurya.cse@gmail.com>
2022-09-23 02:49:12 +0530

    many-to-many

--- src/main/java/com/javatechie/JpaManytomanyApplication.java
+++ src/main/java/com/javatechie/JpaManytomanyApplication.java
@@ -1,13 +1,13 @@
-package com.hipradeep.code;
+package com.javatechie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringProjectsApplication {
+public class JpaManytomanyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringProjectsApplication.class, args);
+		SpringApplication.run(JpaManytomanyApplication.class, args);
 	}
 
 }
--- src/test/java/com/javatechie/JpaManytomanyApplicationTests.java
+++ src/test/java/com/javatechie/JpaManytomanyApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hipradeep.code;
+package com.javatechie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringProjectsApplicationTests {
+class JpaManytomanyApplicationTests {
 
 	@Test
 	void contextLoads() {
