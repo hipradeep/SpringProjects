7fc1935163e282869beac10aeead4162b6d6da84
hipradeep <pradeepmaurya.cse@gmail.com>
2026-05-06 08:29:05 +0530

    Setup Spring 6 MVC non-maven project with required jars

---
+++ src/com/test/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(Model model) {
+        model.addAttribute("message", "Welcome to Spring 6 MVC (Non-Maven)!");
+        return "hello";
+    }
+}
---
+++ web/WEB-INF/views/hello.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Spring 6 MVC</title>
+</head>
+<body>
+    <h1>${message}</h1>
+    <p>This project is configured manually without Maven.</p>
+</body>
+</html>
