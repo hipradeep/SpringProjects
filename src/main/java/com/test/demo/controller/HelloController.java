fb099da28a3893ebd83f0f1bafb590098048500e
hipradeep <pradeepmaurya.cse@gmail.com>
2026-05-06 08:40:09 +0530

    Setup Spring Boot 3 MVC with Thymeleaf view

---
+++ src/main/java/com/test/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.test.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    // @ResponseBody
+    public String sayHello() {
+        return "Hello from Spring Boot 3 with @ResponseBody!";
+    }
+}
